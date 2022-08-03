bae04ebc52df6fc1897610b77d942b331d2dfe43
Diana <divnvp@yandex.com>
2022-08-03 20:48:26 +0300

    add: header component in UIHeader.js

---
+++ src/components/UIHeader.js
@@ -0,0 +1,41 @@
+import { UICard } from "./UICard";
+import { UITaskRow } from "./UITaskRow";
+// Icons
+import calendar from "../pictures/calendar.png"
+
+export function UIHeader(props) {
+  const clicked = (taskId) => {
+    props.select(taskId);
+  }
+
+  const listItems = props.tasks.map((task) =>
+    <UITaskRow
+      key={task.id}
+      taskId={task.id}
+      taskName={task.name}
+      taskStatus={task.status}
+      clicked={clicked}
+    />
+  );
+
+  return (
+    <div className="nav">
+      <div className="nav-row">
+        <img src={calendar} alt="calendar" className="nav-img" />
+        <span className="nav-title">Список</span>
+      </div>
+
+      <UICard>
+        <section className="nav-row">
+          <button className="button_rounded">
+            +
+          </button>
+        </section>
+
+        <nav className="nav__panel">
+          <div>{listItems}</div>
+        </nav>
+      </UICard>
+    </div>
+  );
+}
