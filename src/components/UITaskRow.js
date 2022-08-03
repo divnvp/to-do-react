1a9c4e39862ecdbeb684b6aabec6847105d0d6da
Diana <divnvp@yandex.com>
2022-08-03 20:42:03 +0300

    update: task row clicked

--- src/components/UITaskRow.js
+++ src/components/UITaskRow.js
@@ -1,10 +1,17 @@
 import { UIIcon } from "./UIIcon";
 
 export function UITaskRow(props) {
+  const clicked = () => {
+    props.clicked(props.taskId);
+  }
+
   return (
     <section className="task">
       <UIIcon status={props.taskStatus} />
-      {props.taskName}
+
+      <button onClick={clicked} className="task__button">
+        {props.taskName}
+      </button>
     </section>
   );
 }
